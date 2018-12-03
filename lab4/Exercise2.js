c16efebae7cdb966c9c238c7e1072451636aa7f2
Unknown <htitchonester@gmail.com>
2018-12-02 23:57:02 -0600

    lab4

--- lab4/Exercise2.js
+++ lab4/Exercise2.js
@@ -1,9 +1,30 @@
 //const si = require('systeminformation');
 const os = require('os');
+const {Observable} = require('rxjs');
 
+const obs$ = Observable.create((observer) => {
+    observer.next("Checking you system ...");
+    let flag = true;
+    if (os.totalmem() < 4 * 1024 * 1024 * 1024) {
+        observer.next("This app needs at least 4 GB of RAM");
+        flag = false;
+    }
+    if (os.cpus().length < 2) {
+        observer.next("Processor is not supported");
+        flag = false;
+    }
+   if(flag){
+   setTimeout( () => {observer.complete();} ,1000);
+   }
 
-console.log('Checking your system...');
+});
 
-console.log(os.cpus().length);
-console.log(os.totalmem());
+function checkSystem() {
+    const sub = obs$.subscribe(
+        x => console.log(x),
+        err => console.log(err),
+        () => console.log("System is checked successfully")
+    );
+}
+checkSystem();
 
